44534c17da49f343ad8a1dded45389e9748f45c4
cui <qd_usb@163.com>
2014-11-18 20:27:17 +0800

    simple factory

--- factory/simple_factory.php
+++ factory/simple_factory.php
@@ -6,12 +6,8 @@ class WebFactory{
 		{
 			case 'news':
 				return new NewsWeb();
-			case 'pic':
-				return new PicWeb();
 			case 'display':
 				return new DisplayWeb();
-			default:
-				return new Web();
 		}
 	}
 }
@@ -37,16 +33,38 @@ class PicWeb extends Web{
 		$this->type="pic";
 	}
 }
+class HRWeb extends Web{
+	function HRWeb(){
+		$this->type="hr";
+	}
+}
+class MessageWeb extends Web{
+	function MessageWeb(){
+		$this->type="message";
+	}
+}
 class DisplayWeb extends Web{
 	function DisplayWeb(){
 		$this->type="display";
 	}
 }
-$webs=array("pic","news","display","info");
-foreach($webs as $key=>$val){
-	$factory=new WebFactory();
-	$web=$factory->productWeb($val);
-	$web->loadData("the ".($key+1)." web");
-	$web->display();
+class SimpleWeb{
+	public $webs;
+	public $web_factory;
+	function SimpleWeb(){
+		$this->webs=array("news","display");
+
+	}
+	function createWeb(){
+		$this->web_factory=new WebFactory();
+		foreach($this->webs as $key=>$val){
+			$web=$this->web_factory->productWeb($val);
+			$web->loadData("the ".($key+1)." web");
+			$web->display();
+		}
+
+	}
 }
+$web=new SimpleWeb();
+$web->createWeb();
 
