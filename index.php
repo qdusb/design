6a8be3e3823c47420488d6a9eccbe9742e74ead2
cui <qd_usb@163.com>
2014-11-19 23:34:41 +0800

    index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 Design Pattern
 <br>
-<a href="http://strategy.design" target="_blank">Strategy</a><br>
-<a href="http://observer.design" target="_blank">Observer</a><br>
+<a href="http://www.design.cn/strategy" target="_blank">Strategy</a><br>
+<a href="http://www.design.cn/observer" target="_blank">Observer</a><br>
 <a href="http://www.design.cn/decorator" target="_blank">decorator</a><br>
 <a href="http://www.design.cn/factory/simple_factory.php" target="_blank">Simple Factory</a><br>
 <a href="http://www.design.cn/factory/factory.php" target="_blank">Factory Pattern</a><br>
